f1dee2519a93847ff053f456ed2f7f182c082457
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2024-03-31 11:18:55 -0300

    test: Create City entity class tests
    Co-authored-by: Samagaia <KingHaate@users.noreply.github.com>

---
+++ src/app/entities/city.spec.ts
@@ -0,0 +1,18 @@
+import { City } from './city'
+
+describe(City, () => {
+  describe('#constructor', () =>
+    it('instantiates', () =>
+      expect(new City(1, 'Praia Grande', [])).toBeInstanceOf(City)))
+
+  const city = new City(1, 'Praia Grande', [])
+
+  describe('#id', () => it('returns id', () => expect(city.id).toBe(1)))
+
+  describe('#name', () =>
+    it('returns name', () => expect(city.name).toBe('Praia Grande')))
+
+  describe('#institutions', () =>
+    it('returns institutions', () =>
+      expect(city.institutions).toStrictEqual([])))
+})
