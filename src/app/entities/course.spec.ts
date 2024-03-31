310f8b0171aa0d62ef3e2da48971ca6dd2ca4318
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2024-03-31 11:19:28 -0300

    test: Create Course entity class tests
    Co-authored-by: Samagaia <KingHaate@users.noreply.github.com>

---
+++ src/app/entities/course.spec.ts
@@ -0,0 +1,21 @@
+import { Course } from './course'
+
+describe(Course, () => {
+  describe('#constructor', () =>
+    it('instantiates', () =>
+      expect(new Course(1, 'Ciência de Dados', 6, [])).toBeInstanceOf(Course)))
+
+  const course = new Course(1, 'Ciência de Dados', 6, [])
+
+  describe('#id', () => it('returns id', () => expect(course.id).toBe(1)))
+
+  describe('#name', () =>
+    it('returns name', () => expect(course.name).toBe('Ciência de Dados')))
+
+  describe('#duration', () =>
+    it('returns duration', () => expect(course.duration).toBe(6)))
+
+  describe('#courseOfferings', () =>
+    it('returns courseOfferings', () =>
+      expect(course.courseOfferings).toStrictEqual([])))
+})
