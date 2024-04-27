43fe5e9e3f900700696eb8d6c689a3aeaf53bec0
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2024-04-27 00:56:48 -0300

    feat: Inject PrismaInstitutionsRepository by DatabaseModule

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common'
+
 import { PrismaService } from './prisma/prisma.service'
 
+import { InstitutionsRepository } from '../../app/repositories/institutions-repository'
+import { PrismaInstitutionsRepository } from './prisma/repositories/prisma-institutions-repository'
+
 @Module({
-  providers: [PrismaService],
+  providers: [
+    PrismaService,
+    {
+      provide: InstitutionsRepository,
+      useClass: PrismaInstitutionsRepository,
+    },
+  ],
+  exports: [InstitutionsRepository],
 })
 export class DatabaseModule {}
