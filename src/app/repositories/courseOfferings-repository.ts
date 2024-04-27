4428b28df54df189956e064601d9adacca12b9c9
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2024-04-26 23:39:33 -0300

    feat: Create CourseOfferingsRepository abstract class

---
+++ src/app/repositories/courseOfferings-repository.ts
@@ -0,0 +1,7 @@
+import type { CourseOffering } from '../entities/courseOffering'
+
+export abstract class CourseOfferingsRepository {
+  abstract findManyByInstitutionId(
+    institutionId: number,
+  ): Promise<CourseOffering[]>
+}
