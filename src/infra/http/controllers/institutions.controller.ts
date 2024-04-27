28cdd69be6a7814a86ec8eacb0bc5b6978e1cd40
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2024-04-27 00:53:40 -0300

    feat: Create InstitutionsController

---
+++ src/infra/http/controllers/institutions.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get } from '@nestjs/common'
+
+import { GetInstitutions } from '../../../app/useCases/get-institutions'
+import { InstitutionViewModel } from '../viewModels/institution-view-model'
+
+@Controller('institutions')
+export class InstitutionsController {
+  constructor(private getInstitutions: GetInstitutions) {}
+
+  @Get()
+  async index() {
+    const { institutions } = await this.getInstitutions.execute()
+
+    return { institutions: institutions.map(InstitutionViewModel.toHTTP) }
+  }
+}
