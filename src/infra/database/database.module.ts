7d09cb67ad1ea5a1aa41ed10b7d51bf3a9f6bef3
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2024-04-27 19:33:44 -0300

    feat(DatabaseModule): Add CourseOfferingsRepository and CoursesRepository

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -4,9 +4,13 @@ import { PrismaService } from './prisma/prisma.service'
 
 import { InstitutionsRepository } from '@app/repositories/institutions-repository'
 import { CitiesRepository } from '@app/repositories/cities-repository'
+import { CourseOfferingsRepository } from '@app/repositories/courseOfferings-repository'
+import { CoursesRepository } from '@app/repositories/courses-repository'
 
 import { PrismaInstitutionsRepository } from './prisma/repositories/prisma-institutions-repository'
 import { PrismaCitiesRepository } from './prisma/repositories/prisma-cities-repository'
+import { PrismaCourseOfferingsRepository } from './prisma/repositories/prisma-courseOfferings-repository'
+import { PrismaCoursesRepository } from './prisma/repositories/prisma-courses-repository'
 
 @Module({
   providers: [
@@ -19,7 +23,20 @@ import { PrismaCitiesRepository } from './prisma/repositories/prisma-cities-repo
       provide: CitiesRepository,
       useClass: PrismaCitiesRepository,
     },
+    {
+      provide: CourseOfferingsRepository,
+      useClass: PrismaCourseOfferingsRepository,
+    },
+    {
+      provide: CoursesRepository,
+      useClass: PrismaCoursesRepository,
+    },
   ],
-  exports: [InstitutionsRepository, CitiesRepository],
+  exports: [
+    InstitutionsRepository,
+    CitiesRepository,
+    CourseOfferingsRepository,
+    CoursesRepository,
+  ],
 })
 export class DatabaseModule {}
