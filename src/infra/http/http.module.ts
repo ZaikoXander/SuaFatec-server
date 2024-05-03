f91553571fafe853e02c8e1109986313da1ae159
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2024-05-03 01:45:04 -0300

    feat(HttpModule): Add InstitutionCoursesDataController

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -4,13 +4,19 @@ import { DatabaseModule } from '../database/database.module'
 
 import { InstitutionsController } from './controllers/institutions.controller'
 import { CitiesController } from './controllers/cities.controller'
+import { InstitutionCoursesDataController } from './controllers/institution-courses-data.controller'
 
 import { GetInstitutions } from '@app/useCases/get-institutions'
 import { GetCities } from '@app/useCases/get-cities'
+import { GetInstitutionCoursesData } from '@app/useCases/get-institution-courses-data'
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [InstitutionsController, CitiesController],
-  providers: [GetInstitutions, GetCities],
+  controllers: [
+    InstitutionsController,
+    CitiesController,
+    InstitutionCoursesDataController,
+  ],
+  providers: [GetInstitutions, GetCities, GetInstitutionCoursesData],
 })
 export class HttpModule {}
