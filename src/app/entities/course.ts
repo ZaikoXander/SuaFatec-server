b2f61338e3d72d396810bbec0aa7b0dbf5970ad1
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2024-05-08 23:04:57 -0300

    feat(Course): Add description to entity class

--- src/app/entities/course.ts
+++ src/app/entities/course.ts
@@ -2,11 +2,13 @@ export class Course {
   private _id: number
   private _name: string
   private _duration: number
+  private _description: string
 
-  constructor(id: number, name: string, duration: number) {
+  constructor(id: number, name: string, duration: number, description: string) {
     this._id = id
     this._name = name
     this._duration = duration
+    this._description = description
   }
 
   public get id(): number {
@@ -19,5 +21,9 @@ export class Course {
 
   public get duration(): number {
     return this._duration
+  }
+
+  public get description(): string {
+    return this._description
   }
 }
