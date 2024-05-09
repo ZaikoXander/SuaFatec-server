fcef3b1154e8c4728fdca358f4af9b3037f5ee36
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2024-05-08 23:21:33 -0300

    feat(Institution): Add description field

--- src/app/entities/institution.ts
+++ src/app/entities/institution.ts
@@ -2,6 +2,7 @@ export class Institution {
   private _id: number
   private _name: string
   private _address: string
+  private _description: string[]
   private _cityId: number
   private _latitudeCoordinate: number
   private _longitudeCoordinate: number
@@ -10,6 +11,7 @@ export class Institution {
     id: number,
     name: string,
     address: string,
+    description: string[],
     cityId: number,
     latitudeCoordinate: number,
     longitudeCoordinate: number,
@@ -17,6 +19,12 @@ export class Institution {
     this._id = id
     this._name = name
     this._address = address
+
+    if (description.length !== 2) {
+      throw new Error('Description must have 2 paragraphs')
+    }
+
+    this._description = description
     this._cityId = cityId
     this._latitudeCoordinate = latitudeCoordinate
     this._longitudeCoordinate = longitudeCoordinate
@@ -32,6 +40,10 @@ export class Institution {
 
   public get address(): string {
     return this._address
+  }
+
+  public get description(): string[] {
+    return this._description
   }
 
   public get cityId(): number {
