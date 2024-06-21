49e281c7693dbe14c11dc062dd15f7a3d02f373d
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2024-06-20 23:59:30 -0300

    fix(Institution): Remove description size delimitor

--- src/app/entities/institution.spec.ts
+++ src/app/entities/institution.spec.ts
@@ -1,39 +1,6 @@
 import { Institution } from './institution'
 
 describe(Institution, () => {
-  describe('#constructor', () => {
-    describe('when institution description has 2 paragraphs', () =>
-      it('instantiates', () =>
-        expect(
-          new Institution(
-            1,
-            'Fatec de Praia Grande',
-            'Praça 19 de Janeiro, 144 - Boqueirão, Praia Grande - SP, 11700-100',
-            ['...', '...'],
-            '(13) 3227-6003',
-            1,
-            0,
-            0,
-          ),
-        ).toBeInstanceOf(Institution)))
-
-    describe('when institution description has not 2 paragraphs', () =>
-      it('throws an error', () =>
-        expect(
-          () =>
-            new Institution(
-              1,
-              'Fatec de Praia Grande',
-              'Praça 19 de Janeiro, 144 - Boqueirão, Praia Grande - SP, 11700-100',
-              ['...'],
-              '(13) 3227-6003',
-              1,
-              0,
-              0,
-            ),
-        ).toThrow('Description must have 2 paragraphs')))
-  })
-
   const institution = new Institution(
     1,
     'Fatec de Praia Grande',
@@ -45,34 +12,53 @@ describe(Institution, () => {
     0,
   )
 
-  describe('#id', () => it('returns id', () => expect(institution.id).toBe(1)))
+  describe('#id', () => {
+    it('returns id', () => {
+      expect(institution.id).toBe(1)
+    })
+  })
 
-  describe('#name', () =>
-    it('returns name', () =>
-      expect(institution.name).toBe('Fatec de Praia Grande')))
+  describe('#name', () => {
+    it('returns name', () => {
+      expect(institution.name).toBe('Fatec de Praia Grande')
+    })
+  })
 
-  describe('#address', () =>
-    it('returns address', () =>
+  describe('#address', () => {
+    it('returns address', () => {
       expect(institution.address).toBe(
         'Praça 19 de Janeiro, 144 - Boqueirão, Praia Grande - SP, 11700-100',
-      )))
+      )
+    })
+  })
 
-  describe('#description', () =>
-    it('returns description', () =>
-      expect(institution.description).toStrictEqual(['...', '...'])))
+  describe('#description', () => {
+    it('returns description', () => {
+      expect(institution.description).toStrictEqual(['...', '...'])
+    })
+  })
 
-  describe('#phoneNumber', () =>
-    it('returns phone number', () =>
-      expect(institution.phoneNumber).toBe('(13) 3227-6003')))
+  describe('#phoneNumber', () => {
+    it('returns phone number', () => {
+      expect(institution.phoneNumber).toBe('(13) 3227-6003')
+    })
+  })
 
-  describe('#cityId', () =>
-    it('returns city id', () => expect(institution.cityId).toBe(1)))
+  describe('#cityId', () => {
+    it('returns city id', () => {
+      expect(institution.cityId).toBe(1)
+    })
+  })
 
-  describe('#latitudeCoordinate', () =>
-    it('returns latitude coordinate', () =>
-      expect(institution.latitudeCoordinate).toBe(0)))
+  describe('#latitudeCoordinate', () => {
+    it('returns latitude coordinate', () => {
+      expect(institution.latitudeCoordinate).toBe(0)
+    })
+  })
 
-  describe('#longitudeCoordinate', () =>
-    it('returns longitude coordinate', () =>
-      expect(institution.longitudeCoordinate).toBe(0)))
+  describe('#longitudeCoordinate', () => {
+    it('returns longitude coordinate', () => {
+      expect(institution.longitudeCoordinate).toBe(0)
+    })
+  })
 })
